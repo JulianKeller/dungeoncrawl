d21c9af904ab9aa0b2fa02824d35b3815faa5306
Jacob Pugsley <jacob.pugsley@wsu.edu>
2019-10-30 14:14:11 -0700

    added method to reset database

--- dungeoncrawl/src/ItemManager.java
+++ dungeoncrawl/src/ItemManager.java
@@ -38,6 +38,8 @@ public class ItemManager {
 	Statement s;
 	ResultSet rs;
 	
+	String dbName = "dungeoncrawl";
+	
 	private void dbConnect() throws SQLException{
 		//set up data source
 		if( datasource == null ){
@@ -53,6 +55,9 @@ public class ItemManager {
 		
 		//create a statement to execute queries with
 		s = conn.createStatement();
+		
+		String q = "use "+dbName;
+		s.executeQuery(q);
 	}
 	
 	private void dbClose() throws SQLException{
@@ -61,10 +66,54 @@ public class ItemManager {
 		conn.close();
 	}
 	
+	private void dbReset() throws SQLException{
+		if( datasource == null ){
+			System.out.println("Cannot reset null database.");
+			return;
+		}
+		
+		String q = "drop database "+dbName;
+		s.executeUpdate(q);
+		q = "create database "+dbName;
+		s.executeUpdate(q);
+		q = "use "+dbName;
+		s.executeUpdate(q);
+		
+		q = "create table Player("+
+		"pid int,"+				
+		"pname varchar(200),"+	
+		"pclass varchar(200),"+	
+		"plevel smallint,"+		
+		"primary key(pid)"+
+		");";
+		s.executeUpdate(q);
+
+		q = "create table Item("+
+		"iid int,"+				
+		"oid int,"+				
+		"ieffect varchar(200),"+	
+		"itype varchar(200),"+		
+		"imat varchar(200),"+		
+    
+    	"primary key(iid),"+
+    	"foreign key(oid) references player(pid)"+
+    	"on update cascade on delete cascade"+
+    	");";
+		s.executeUpdate(q);
+		
+		//insert world as player
+		q = "insert into player values(0, '', '', 0)";
+		s.executeUpdate(q);
+	}
+	
 	public ItemManager(Main game) throws SQLException{
 		dbConnect(); //connect to data source
+		dbReset();
 		this.game = game;
 		rand = new Random();
+		
+		//testing
+		plant(5);
 	}
 	
 	public void give(int itemID, int playerID) throws SQLException{
@@ -81,7 +130,7 @@ public class ItemManager {
 	
 	private int currentItemID = 0;
 	
-	public void plant() throws SQLException{
+	public void plant(int numItems) throws SQLException{
 		//add a new item to the map at random position
 		int maxx = game.ScreenWidth/game.tileW;
 		int maxy = game.ScreenHeight/game.tileH;
@@ -90,16 +139,21 @@ public class ItemManager {
 		
 		Vector vec = new Vector(rand.nextInt(maxx), rand.nextInt(maxy));
 		
-		//create a new item object
-		Item item = new Item(vec, false, currentItemID, 0);
-		currentItemID++;
-		
-		//add its values to the database
-		String q = "insert into item values("
-				+currentItemID+","+item.getOID()+","+item.getEffect()+","+item.getType()+","+item.getMaterial()
-				+")";
-		
-		rs = s.executeQuery(q);
+		while( numItems > 0 ){
+			//create a new item object with random properties
+			Item item = new Item(vec, false, currentItemID, 0);
+			currentItemID++;
+			
+			//add its values to the database
+			String q = "insert into item values("
+					+currentItemID+","+item.getOID()+",'"+item.getEffect()+"','"+item.getType()+"','"+item.getMaterial()
+					+"')";
+			
+			System.out.println("Query = "+q);
+			s.executeUpdate(q);
+			
+			numItems--;
+		}
 	}
 	
 	public void restore(int itemID) throws SQLException{
