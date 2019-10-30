0d222705d70404acf9bdfb37ec43d8b9d416e762
Tyler Higgins <tyler.higgins@wsu.edu>
2019-10-30 13:45:50 -0700

    Added methods to MovingEntity
    Added methods for adding/subtracting/getting MovingEntity's speed, added an Overloaded constructor that sets worldCoordinates as a JIG Vector.

--- dungeoncrawl/src/MovingEntity.java
+++ dungeoncrawl/src/MovingEntity.java
@@ -5,23 +5,36 @@ public class MovingEntity extends Entity {
     private int hitPoints;
     private int armorPoints;
     private Vector worldCoordinates;
-    private float speed;
+    private int speed;
 
     /**
-     * Create a new Entity
+     * Create a new Entity (x,y)
      * @param wx starting world x coordinate
      * @param wy starting world y coordinate
      * @param hp entity's starting hit points
      * @param ap entity's starting armor points
      * @param spd entity's starting speed.
      */
-    public MovingEntity(final float wx, final float wy, int hp, int ap, float spd) {
+    public MovingEntity(final float wx, final float wy, int hp, int ap, int spd) {
         hitPoints = hp;
         armorPoints = ap;
         worldCoordinates = new Vector(wx,wy);
         speed = spd;
     }
 
+    /**
+     * Create a new Entity (Vector)
+     * @param wc Vector for starting world coordinates of the moving entity
+     * @param hp entity's starting hit points
+     * @param ap entity's starting armor points
+     * @param spd entity's starting speed.
+     */
+    public MovingEntity(Vector wc, int hp, int ap, int spd){
+        hitPoints = hp;
+        armorPoints = ap;
+        worldCoordinates = wc;
+        speed = spd;
+    }
     /**
      * Retrieve current hit points.
      * @return current hit points
@@ -46,6 +59,18 @@ public class MovingEntity extends Entity {
         hitPoints-= amt;
     }
 
+
+    public Vector getWorldCoordinates(){
+        return worldCoordinates;
+    }
+
+    /**
+     * Sets the moving entity's worldcoordinates
+     * @param wc Vector to set entity's world coordinates.
+     */
+    public void setWorldCoordinates(Vector wc){
+        worldCoordinates = wc;
+    }
     /**
      * Adds hit points by specified amount
      * @param amt amount to add hit points by
@@ -69,4 +94,19 @@ public class MovingEntity extends Entity {
         armorPoints += amt;
     }
 
+    /**
+     * Adds to Moving entity's speed.
+     * @param amt amount to add to speed.
+     */
+    public void addToSpeed(int amt){
+        speed += amt;
+    }
+
+    /**
+     *
+     * @param amt subtract amount to speed.
+     */
+    public void subSpeed(int amt){
+        speed -= amt;
+    }
 }
