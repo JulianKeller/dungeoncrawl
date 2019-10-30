5fd1a9a8b1c60a9cc8be2c0c1a9daaa355b9d043
Jacob Pugsley <jacob.pugsley@wsu.edu>
2019-10-29 17:55:57 -0700

    added initialization functions to StationaryObject

--- dungeoncrawl/src/StationaryObject.java
+++ dungeoncrawl/src/StationaryObject.java
@@ -1,4 +1,23 @@
+import jig.Entity;
+import jig.Vector;
 
-public class StationaryObject {
-
+public class StationaryObject extends Entity{
+	/**
+	 * Represents an object which remains in the same position on the level.
+	 */
+	private Vector worldCoordinates; //the object's coordinates on the world (not screen coordinates)
+	private boolean locked;
+	
+	public StationaryObject(Vector worldCoordinates, boolean locked){
+		this.worldCoordinates = worldCoordinates;
+		this.locked = locked;
+	}
+	
+	public Vector getWorldCoordinates(){
+		return worldCoordinates;
+	}
+	
+	public boolean isLocked(){
+		return locked;
+	}
 }
