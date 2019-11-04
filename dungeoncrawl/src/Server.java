e3139bbd49610cc619f64649fc2ad1b7b786220a
Tyler Higgins <tyler.higgins@wsu.edu>
2019-11-03 18:21:16 -0800

    finished issue4

--- dungeoncrawl/src/Server.java
+++ dungeoncrawl/src/Server.java
@@ -4,8 +4,46 @@ import java.util.Scanner;
 
 
 public class Server {
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+    public Server(int port) {
+        // starts server and waits for connection
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+            System.out.println("Server Started");
+            System.out.println("Waiting for a client...");
+            socket = server.accept();
+            System.out.println("Client accepted");
 
+            // takes input from the client socket
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            String line = "";
+
+            // reads message from client until "Over" is sent
+            while (!line.equals("Over")) {
+                try {
+                    line = in.readUTF();
+                    System.out.println((line));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Closing connection");
+            // close connection
+            socket.close();
+            in.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    }
     public static void main(String [] args){
-
+        Server server = new Server(5000);
     }
 }
--- issues.md
+++ issues.md
@@ -2,9 +2,9 @@
 **[ 1 ]** Create Moving Entity Class<br>
 **[ 2 ]** Create Character Class<br>
 **[ 3 ]** Create Enemy Class<br>
-## PENDING ISSUES ##
 **[ 4 ]** Create a simple client/server network<br>
+## PENDING ISSUES ##
 **[ 5 ]** Convert to a multithreaded network.<br>
 **[ 6 ]** Work on the server as the game<br>
-**[ 7 ]** Use a semi-dumb client model<br>
+**[ 7 ]** Use a dumb client model<br>
 **[ 8 ]** Test connections on lab machine<br> 
