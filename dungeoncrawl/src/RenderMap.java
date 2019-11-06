8372cb9b3bdade9d8d550610e171482da9275fcc
Julian Keller <julian.keller@wsu.edu>
2019-11-06 10:43:45 -0800

    fixed random map IO error

--- dungeoncrawl/src/RenderMap.java
+++ dungeoncrawl/src/RenderMap.java
@@ -1,5 +1,6 @@
 import jig.Entity;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,7 +15,10 @@ public class RenderMap extends Entity {
 
     // grabs a random map and returns it as a 2d array
     public static int[][] getRandomMap(Main dc) throws IOException {
-        String directory = "dungeoncrawl/mapGen/maps/";
+        File f = new File("dungeoncrawl/mapGen/maps");
+        // Get the absolute path of file f
+        String absolute = f.getAbsolutePath();
+        String directory = absolute + "/";
         Random r = new Random();
         int rand = r.nextInt(100);
         String filepath = directory + "map" + rand + ".txt";
