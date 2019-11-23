3bd98a027c14e439bf19fba351a41523a7871fa5
Jacob Pugsley <jacob.pugsley@wsu.edu>
2019-11-22 18:13:43 -0800

    added all character effects and basic implementations

--- dungeoncrawl/src/client/MovingEntity.java
+++ dungeoncrawl/src/client/MovingEntity.java
@@ -6,11 +6,24 @@ import jig.Vector;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Random;
+
+import org.newdawn.slick.SlickException;
 
 public class MovingEntity extends Entity {
     private int hitPoints;
+    private final int startingHitPoints;
     private int armorPoints;
     private int mana;
+    private int strength; //determines what level of items the player can pick up
+    
+    //boolean effects for AI
+    private boolean invisible = false;
+    private boolean stinky = false;
+    private boolean thorny = false;
+    private boolean frightening = false;
+    private boolean reflecting = false;
+    
     private Vector worldCoordinates;
     private int speed;
     private int pid;
@@ -18,6 +31,11 @@ public class MovingEntity extends Entity {
     private ArrayList<Item> codex; //list of identified items
     private Item [] equipped;
     private Vector position;
+    
+    //random number generator
+    private Random rand = new Random();
+    
+   
     
     private ArrayList<Effect> activeEffects; //list of things currently affecting the character
     private final int defaultEffectTimer = 5000;
@@ -37,10 +55,12 @@ public class MovingEntity extends Entity {
     public MovingEntity(final float wx, final float wy, int pid) {
         super(wx, wy);
         hitPoints = 0;
+        startingHitPoints = hitPoints;
         armorPoints = 0;
         worldCoordinates = new Vector(wx, wy);
         speed = 0;
         mana = 0;
+        strength = 1;
         this.pid = pid;
         inventory = new ArrayList<Item>(10);
         equipped = new Item[4];
@@ -64,15 +84,34 @@ public class MovingEntity extends Entity {
     public MovingEntity(Vector wc, int pid) {
         super(wc.getX(), wc.getY());
         hitPoints = 0;
+        startingHitPoints = hitPoints;
         armorPoints = 0;
         worldCoordinates = wc;
         speed = 0;
         mana = 0;
+        strength = 1;
         this.pid = pid;
         inventory = new ArrayList<Item>(10);
         equipped = new Item[5];
         Arrays.fill(equipped, null);
         position = new Vector(0,0);
+    }
+    
+    //get boolean effects for AI
+    public boolean isInvisible(){
+    	return invisible;
+    }
+    public boolean isStinky(){
+    	return stinky;
+    }
+    public boolean isThorny(){
+    	return thorny;
+    }
+    public boolean isFrightening(){
+    	return frightening;
+    }
+    public boolean isReflecting(){
+    	return reflecting;
     }
     
     public void addEffect(String name){
@@ -89,6 +128,140 @@ public class MovingEntity extends Entity {
     
     public ArrayList<Effect> getActiveEffects(){
     	return activeEffects;
+    }
+    
+    public void updateEffectTimers(int delta){
+    	//reduce each active effect timer by delta
+    	for( Effect e : activeEffects ){
+    		e.timer -= delta;
+    	}
+    	//remove any expired effects
+    	activeEffects.removeIf(b -> b.timer <= 0);
+    }
+    
+    /*
+     * Healing:
+	Restore 25% of lost HP
+	Staff: on hit
+	Potion: on hit/drink
+Strength:
+	Increase character strength by 1 point.
+Flame (Flaming):
+	Decrease health by 10% every update loop.
+Mana:
+	Increase maximum mana by 15%
+Invisibility:
+	Enemies can no longer see character.
+Poisoned:
+	Decrease health by 5% every update loop.
+Ice:
+	Character is frozen in place for a time.
+Lightning:
+	Character has a 30% chance of being struck by lightning after a hit.
+Stench:
+	Enemies have a 30% chance to run away from the character.
+Iron Skin:
+	Defense attribute is doubled.
+Thorns:
+	Attacking enemies will take 50% of the damage they deal.
+Swiftness:
+	Character speed is doubled.
+Fright:
+	Enemies have a 50% chance to run away.
+Might:
+	Double sword damage.
+Regeneration:
+	50% chance to Restore 3% of lost HP each loop.
+Reflection:
+	Attacking enemies will take 50% of the damage they deal.
+     */
+    
+    public void implementEffects() throws SlickException{
+    	for( Effect e : activeEffects ){
+    		if( e.name.equals("Healing") ){
+    			//find the difference between this entity's current HP
+    			//  and its starting HP, then add 25% of that to the
+    			//  current value
+    			int diff = startingHitPoints - hitPoints;
+    			hitPoints += (hitPoints*0.25);
+    			
+    		}else if( e.name.equals("Strength") ){
+    			//increment the player's strength variable
+    			strength++;
+    			//this should only happen once
+    			
+    		}else if( e.name.equals("Flame") ){
+    			//decrease health by 10
+    			hitPoints -= 10;
+    			
+    		}else if( e.name.equals("Mana") ){
+    			//add 15% to the current maximum mana
+    			mana += (mana*0.15);
+    			
+    		}else if( e.name.equals("Invisibility") ){
+    			//little too complicated for this function,
+    			//  just set a boolean value
+    			invisible = true;
+    		}else if( e.name.equals("Poisoned") ){
+    			//decrease health by 5
+    			hitPoints -= 5;
+    			
+    		}else if( e.name.equals("Ice") ){
+    			//set movement speed to zero
+    			speed = 0;
+    		}else if( e.name.equals("Lightning") ){
+    			//roll 30% change to take 20 damage
+    			rand.setSeed(System.nanoTime());
+    			int r = rand.nextInt(100);
+    			if( r < 30 ){
+    				hitPoints -= 20;
+    			}
+    			
+    		}else if( e.name.equals("Stench") ){
+    			//another AI problem, set a boolean
+    			stinky = true;
+    			
+    		}else if( e.name.equals("Iron Skin") ){
+    			//double the armor points variable
+    			armorPoints *= 2;
+    			
+    		}else if( e.name.equals("Thorns") ){
+    			//AI problem
+    			thorny = true;
+    			
+    		}else if( e.name.equals("Swiftness") ){
+    			//double movement speed
+    			speed *= 2;
+    			
+    		}else if( e.name.equals("Fright") ){
+    			//AI problem, see stench
+    			frightening = true;
+    			
+    		}else if( e.name.equals("Might") ){
+    			//double player's attack damage
+    			//TODO: add attack system
+    			
+    		}else if( e.name.equals("Regeneration") ){
+    			//roll 50% chance to restore 3 hp
+    			rand.setSeed(System.nanoTime());
+    			int r = rand.nextInt(100);
+    			if( r < 50 ){
+    				hitPoints += 3;
+    			}
+    			
+    		}else if( e.name.equals("Reflection") ){
+    			//AI problem, see thorns
+    			reflecting = true;
+    			
+    		}else{
+    			throw new SlickException("Unknown character effect.");
+    		}
+    		
+    		//remove one-time effects
+    		activeEffects.removeIf(b -> b.name.equals("Strength") || 
+    									b.name.equals("Iron Skin") || 
+    									b.name.equals("Healing"));
+    	}
     }
 
     public void addItem(Item i){
@@ -218,6 +391,7 @@ public class MovingEntity extends Entity {
             }
         }
     }
+    
 
     /**
      * Unequips the item from client.MovingEntity's equipped list and places it in inventory.
